42f57c88e5894ec26298c2ff67249c2288448aef
dinesh nangare <dineshnangare1717@gmail.com>
2022-04-21 20:20:12 +0530

    inforn final mood

---
+++ src/main/java/com/bridgelabz/MoodAnalysisException.java
@@ -0,0 +1,14 @@
+package com.bridgelabz;
+
+public class MoodAnalysisException extends Exception {
+    public enum Exception_Type {
+        NULL, EMPTY
+    }
+
+    public Exception_Type type;
+
+    public MoodAnalysisException(String message, Exception_Type type) {
+        super(message);
+        this.type=type;
+    }
+}
